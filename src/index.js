bb82e63cf153a640b2b3d7558e2c4af20cf6f488
guoyongwei <guoyongwei@gomefinance.com.cn>
2020-01-10 17:31:00 +0800

    build ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from './app.vue';
 
+import './assets/images/1.jpeg';
+import "./assets/css/app.css"
+
 const root = document.createElement("div");
 document.body.appendChild(root);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,48 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-module.exports = {
+module.exports = function(env, arg) {
+  return {
+    mode: env.production ? 'production' : 'development',
     entry: path.resolve(__dirname, "src/index.js"),
     output: {
-        chunkFilename: "[name].[hash:6].js",
-        sourceMapFilename: "[name]-[chunkhash].map",
-        path: path.resolve(__dirname, "dist")
+      filename: "[name].[hash:6].bundle.js",
+      sourceMapFilename: "[name]-[chunkhash].map",
+      path: path.resolve(__dirname, "dist")
     },
     module: {
         rules: [
             {
-                test: "/\.vue$/",
-                use: "vue-loader"
-            }
+              test: /\.vue$/,
+              use: "vue-loader"
+            },
+            {
+              test: /\.css$/,
+              use: [
+                "style-loader",
+                "css-loader",
+              ]
+            },
+            {
+              test: /\.(jpg|png|gif|bmp|jpeg)$/,
+              use: {
+                loader: "url-loader",
+                // "url-loader?limit=8000"
+                options: {
+                  limit: 1024,
+                  name: "[name].[ext]"
+                }
+              }
+            },
         ]
-    }
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.resolve(__dirname, "./index.html")
+      }),
+      new VueLoaderPlugin()
+    ]
+  }
 }
